8937d6fc67209a299e24768caa8c4c74925daa1b
Fabiano <fabinppk@hotmail.com>
2019-08-07 19:15:54 -0300

    feat: baseURL dinamica

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const dev = process.env.NODE_ENV !== 'production';
+
+const baseURL = dev === 'production'? "https://tindev-backend.herokuapp.com" : "http://localhost:3333";
+
 const api = axios.create({
-  baseURL: "http://localhost:3333"
+  baseURL: baseURL
 });
 
 export default api;
