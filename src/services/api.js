504fba48a2e9c6121582c0b4ba635a3cb8de1d65
Fabiano <fabinppk@hotmail.com>
2019-08-07 19:30:30 -0300

    console log

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.REACT_APP_NODE_ENV || 'development';
+
+console.log(process.env.REACT_APP_NODE_ENV, process.env.NODE_ENV);
 
 const baseURL = dev === 'production'? "https://tindev-backend.herokuapp.com" : "http://localhost:3333";
 
