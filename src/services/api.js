da8013011edd6fffdddb7cce99909c6ac2d283df
Fabiano <fabinppk@hotmail.com>
2019-08-07 19:33:51 -0300

    baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const dev = process.env.REACT_APP_NODE_ENV || 'development';
 
-console.log(process.env.REACT_APP_NODE_ENV, process.env.NODE_ENV);
-
 const baseURL = dev === 'production'? "https://tindev-backend.herokuapp.com" : "http://localhost:3333";
+
+console.log(baseURL)
 
 const api = axios.create({
   baseURL: baseURL
