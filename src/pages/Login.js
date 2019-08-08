670b886c40313061effe2c7b37ad5b16329e1f48
Fabiano <fabinppk@hotmail.com>
2019-08-08 10:57:30 -0300

    feat: loading / prettier / eslint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,37 +1,37 @@
-import React, { useState } from "react";
-import api from "../services/api";
-import "./Login.css";
+import React, { useState } from 'react';
+import api from '../services/api';
+import './Login.css';
 
-import logo from "../assets/logo.svg";
+import logo from '../assets/logo.svg';
 
-function Login({ history }) {
-  const [username, setUsername] = useState("");
+const Login = ({ history }) => {
+    const [username, setUsername] = useState('');
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    const response = await api.post("/devs", {
-      username
-    });
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const response = await api.post('/devs', {
+            username
+        });
 
-    const { _id } = response.data;
+        const { _id } = response.data;
 
-    history.push(`/dev/${_id}`);
-  }
+        history.push(`/dev/${_id}`);
+    }
 
-  return (
-    <div className="login-container">
-      <form onSubmit={handleSubmit}>
-        <img src={logo} alt="Tindev" />
-        <input
-          placeholder="Digite seu usuário do github"
-          type="text"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        />
-        <button type="submit">Entrar</button>
-      </form>
-    </div>
-  );
-}
+    return (
+        <div className="login-container">
+            <form onSubmit={handleSubmit}>
+                <img src={logo} alt="Tindev" />
+                <input
+                    placeholder="Digite seu usuário do github"
+                    type="text"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <button type="submit">Entrar</button>
+            </form>
+        </div>
+    );
+};
 
 export default Login;
--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
 const dev = process.env.REACT_APP_NODE_ENV || 'development';
 
-const baseURL = dev === 'production'? "https://tindev-backend.herokuapp.com" : "http://localhost:3333";
-
-console.log(baseURL)
+const baseURL =
+    dev === 'production' ? 'https://tindev-backend.herokuapp.com' : 'http://localhost:3333';
 
 const api = axios.create({
-  baseURL: baseURL
+    baseURL
 });
 
 export default api;
