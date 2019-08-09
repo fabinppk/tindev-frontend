61eb2f883e5f2bf24cedf956b85f4eb660ae7220
Fabiano <fabinppk@hotmail.com>
2019-08-09 13:52:11 -0300

    feat: favicon + error handle

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,22 +6,29 @@ import logo from '../assets/logo.svg';
 
 const Login = ({ history }) => {
     const [username, setUsername] = useState('');
+    const [error, setError] = useState(null);
 
     async function handleSubmit(e) {
         e.preventDefault();
-        const response = await api.post('/devs', {
-            username
-        });
+        await api
+            .post('/devs', {
+                username
+            })
+            .then(response => {
+                const { _id } = response.data;
 
-        const { _id } = response.data;
-
-        history.push(`/dev/${_id}`);
+                history.push(`/dev/${_id}`);
+            })
+            .catch(() => {
+                setError('Houston we have a problem');
+            });
     }
 
     return (
         <div className="login-container">
             <form onSubmit={handleSubmit}>
                 <img src={logo} alt="Tindev" />
+                {error && <p className="error">{error}</p>}
                 <input
                     placeholder="Digite seu usuário do github"
                     type="text"
